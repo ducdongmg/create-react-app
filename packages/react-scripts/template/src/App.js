372c853e9c0d53202d60e75ec4df0f371faff7e3
Thai Nguyen <nguyen7thai@gmail.com>
2017-12-01 17:25:20 +0700

    Wtf prettier?

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import codelink from './assets/img/codelink.jpg';
-import './App.scss';
+import React, { Component } from 'react'
+import codelink from './assets/img/codelink.jpg'
+import './App.scss'
 
 class App extends Component {
   render() {
     return (
-      <div styleName="app">
-        <header styleName="app-header">
-          <img src={codelink} styleName="app-logo" alt="logo" />
-          <h1 styleName="app-title">Welcome to CodeLink Training Program</h1>
+      <div styleName='app'>
+        <header styleName='app-header'>
+          <img src={codelink} styleName='app-logo' alt='logo' />
+          <h1 styleName='app-title'>Welcome to CodeLink Training Program</h1>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
